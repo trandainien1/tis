c15bcb22fd2ba438610ea9a2eb5d83d8065f0dc8
trandainien1 <trandainien1@gmail.com>
2025-02-26 19:51:08 +0700

    update

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,2 +1,3 @@
 print('DEBUG: Albu, Bboxes created!!!')
-from .ilsvrc2012 import AlbumentationsImageNet, BboxesImageNet
+from .ilsvrc2012 import AlbumentationsImageNet
+from .ilsvrc20121 import BboxesImageNet
