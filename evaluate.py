be3bc365aa8086128b152f067491454839d14dbf
trandainien1 <trandainien1@gmail.com>
2025-02-27 19:18:40 +0700

    update

--- evaluate.py
+++ evaluate.py
@@ -128,7 +128,8 @@ def main(cfg: DictConfig):
         # -----------------------------------------
 
         if cfg.metric.npz_only:
-            saliency_map = saliency_maps[idx - cfg.start_idx] # !Nien: subtract number of images here
+            # saliency_map = saliency_maps[idx - cfg.start_idx] # !Nien: subtract number of images here
+            saliency_map = saliency_maps[idx] # !Nien: subtract number of images here
             saliency_map = saliency_map.reshape((1, 1, *saliency_map.shape))
             if saliency_map.shape != image.shape:
                 saliency_map = upsampling_fn(saliency_map)
