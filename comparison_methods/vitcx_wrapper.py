087b75044f5ed8f1ff7ea850930a696dfd7c20f3
trandainien1 <trandainien1@gmail.com>
2025-03-01 12:08:02 +0700

    update

--- comparison_methods/vitcx_wrapper.py
+++ comparison_methods/vitcx_wrapper.py
@@ -7,7 +7,7 @@ import copy
 
 sys.path.append("comparison_methods/ViTCX/ViT_CX")
 
-from ViT_CX.ViT_CX import ViT_CX, reshape_function_vit
+from comparison_methods.ViTCX.ViT_CX import ViT_CX, reshape_function_vit
 
 from torchvision.models import VisionTransformer as VisionVIT
 from timm.models.vision_transformer import VisionTransformer as TimmVIT
